ac437c6a07d1c910f037551611aa414bb49f5c3e
Rocky Khan <rockydottkhan@gmail.com>
2013-07-15 04:15:37 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,11 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer;
+
+fs.readFileSync("index.html", function(err, data) {
+  if (err) throw err;
+  buffer = new Buffer(data);
+  //console.log(data);
+});
+
 app.get('/', function(request, response) {
-  var b = new Buffer(fs.readFileSync("index.html", function(err, data) {
-	if (err) throw err;
-	console.log(data);
-	})), "utf-8");
   response.send(b.toString('utf-8');
   //response.send('Hello World 2!');
 });
@@ -15,3 +19,5 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
